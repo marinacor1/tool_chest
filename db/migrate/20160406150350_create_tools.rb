ecf364e88c9dd10cacfdba10c9b52991a2621968
marinacor1 <marinaspam1234@gmail.com>
2016-04-06 09:59:41 -0600

    adding tools

--- db/migrate/20160406150350_create_tools.rb
+++ db/migrate/20160406150350_create_tools.rb
@@ -2,7 +2,7 @@ class CreateTools < ActiveRecord::Migration
   def change
     create_table :tools do |t|
       t.text :name
-      t.decimal :price
+      t.decimal :price, precision: 20, scale: 2 #scale refers to the numbers of digitals 
       t.integer :quantity
 
       t.timestamps null: false
