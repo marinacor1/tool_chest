f76723684fe4e6ecc125b2e5d0cc86302a3b5fb8
marinacor1 <marinaspam1234@gmail.com>
2016-04-13 19:36:34 -0600

    working on sad path for user registration

--- test/features/user_can_login_to_page_and_create_new_login_test.rb
+++ test/features/user_can_login_to_page_and_create_new_login_test.rb
@@ -13,13 +13,12 @@ class UserCanLoginToPageAndCreateNewLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "user will not create login with no name" do
-    skip
     visit new_user_path
 
     fill_in "Username", with: nil
     fill_in "Email", with: "marina@email.com"
     fill_in "Password", with: "password"
-    fill_in "Password Confirmation", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_button "Create Account"
 
     assert_equal new_user_path, current_path
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
-
+require 'minitest/pride'
 class ActiveSupport::TestCase
   fixtures :all
 end
 
 class ActionDispatch::IntegrationTest
-  include Capybara::DSL 
+  include Capybara::DSL
 end
